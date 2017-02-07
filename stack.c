1d517c45ce849bff4bcb23fdd3307473fcf7f1c4
zzihyeon <guswldb12@naver.com>
2017-02-07 11:47:46 +0900

    STack

---
+++ stack.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+int Stack[10];
+int main(){
+	int i=0,select;
+
+	printf("push : 1 pop : 2 end : 3\n");
+	scanf("%d",&select);
+	while(1){
+		if(select==1){
+			if(i<9){
+				printf("push :");
+				scanf("%d",&Stack[i]);
+			}
+			else{
+				printf("too many\n");
+			}
+		}
+		else if(select==2){
+			if(i<0){
+				printf("Nothing to pop\n");
+			}
+			else{
+				printf("pop: %d\n",Stack[i]);
+			}
+		}
+		else break;
+	}
+	return 0;
+}
